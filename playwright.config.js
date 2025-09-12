7ac343c46d15cd16460db3e02b08250d555bf663
Abdul Muntaqim <abdulmuntaqim120@gmail.com>
2025-09-12 22:31:47 +0500

    Config Headless change

--- playwright.config.js
+++ playwright.config.js
@@ -47,7 +47,7 @@ export default defineConfig({
       name: 'chromium',
       use: {
         launchOptions: {
-          headless: false,
+          headless: true,
           args: ['--start-maximized']
         },
         viewport: {width:1920, height:1080}, // disables default viewport
